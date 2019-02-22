eabfff99d88ca47c3c43685c83ae0a55ed4b87fe
Yanay Zabary <yanay.tsabary@gmail.com>
2019-02-23 08:47:34 +1100

    Polished CreateUserActivity

--- app/src/main/java/co/getdere/chatapp/Controller/CreateUserActivity.kt
+++ app/src/main/java/co/getdere/chatapp/Controller/CreateUserActivity.kt
@@ -1,13 +1,20 @@
 package co.getdere.chatapp.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import co.getdere.chatapp.R
 import co.getdere.chatapp.Services.AuthService
 import co.getdere.chatapp.Services.UserDataService
+import co.getdere.chatapp.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -20,6 +27,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        create_spinner.visibility = View.INVISIBLE
 
     }
 
@@ -60,20 +68,64 @@ class CreateUserActivity : AppCompatActivity() {
         val email = create_email_text.text.toString()
         val password = create_password_text.text.toString()
 
-        AuthService.registerUser(this, email, password) { registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) { loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvater, avatarColor){createSuccess -> if (createSuccess){
-                            println(UserDataService.avatarName)
-                            println(UserDataService.avatarColor)
-                            println(UserDataService.name)
-                            finish()
-                        }}
+        if (userName.length > 3) {
+            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                enableSpinner(true)
+                AuthService.registerUser(this, email, password) { registerSuccess ->
+                    if (registerSuccess) {
+                        AuthService.loginUser(this, email, password) { loginSuccess ->
+                            if (loginSuccess) {
+                                AuthService.createUser(
+                                    this,
+                                    userName,
+                                    email,
+                                    userAvater,
+                                    avatarColor
+                                ) { createSuccess ->
+                                    if (createSuccess) {
+                                        enableSpinner(false)
+                                        finish()
+
+                                        val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                        LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+
+                                    } else {
+                                        errorToast()
+                                    }
+                                }
+                            } else {
+                                errorToast()
+                            }
+                        }
+                    } else {
+                        errorToast()
                     }
                 }
+            } else {
+                Toast.makeText(this, "Please enter a valid email.", Toast.LENGTH_LONG).show()
             }
+        } else {
+            Toast.makeText(this, "Name is too short.", Toast.LENGTH_LONG).show()
         }
     }
 
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+
+        if (enable) {
+            create_spinner.visibility = View.VISIBLE
+
+        } else {
+            create_spinner.visibility = View.INVISIBLE
+        }
+        create_create_user_btn.isEnabled = !enable
+        create_avatar_image_view.isEnabled = !enable
+        create_background_color_btn.isEnabled = !enable
+
+    }
 }
