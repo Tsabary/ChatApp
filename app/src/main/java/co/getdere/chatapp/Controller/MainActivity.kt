864f66b1cdceb30fe45552c677acdccce0aa6f62
Yanay Zabary <yanay.tsabary@gmail.com>
2019-02-23 18:05:31 +1100

    Connected broadcast recievers

--- app/src/main/java/co/getdere/chatapp/Controller/MainActivity.kt
+++ app/src/main/java/co/getdere/chatapp/Controller/MainActivity.kt
@@ -1,14 +1,23 @@
 package co.getdere.chatapp.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import co.getdere.chatapp.R
+import co.getdere.chatapp.Services.AuthService
+import co.getdere.chatapp.Services.UserDataService
+import co.getdere.chatapp.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +34,26 @@ class MainActivity : AppCompatActivity() {
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            userDataChangeReceiver, IntentFilter(
+                BROADCAST_USER_DATA_CHANGE
+            )
+        )
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                user_name_nav_header.text = UserDataService.name
+                user_email_nav_header.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                user_image_nav_header.setImageResource(resourceId)
+                user_image_nav_header.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+
+                login_button_nav_header.text = "Log out"
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -36,8 +65,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun LoginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+
+        if (AuthService.isLoggedIn) {
+            UserDataService.logOut()
+            login_button_nav_header.text = "Login"
+            user_email_nav_header.text = ""
+            user_name_nav_header.text = ""
+            user_image_nav_header.setImageResource(R.drawable.profiledefault)
+            user_image_nav_header.setBackgroundColor(Color.TRANSPARENT)
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+
     }
 
 
--- app/src/main/java/co/getdere/chatapp/Services/UserDataService.kt
+++ app/src/main/java/co/getdere/chatapp/Services/UserDataService.kt
@@ -1,11 +1,46 @@
 package co.getdere.chatapp.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
-    var id =""
-    var avatarColor =""
-    var avatarName =""
-    var email =""
-    var name =""
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+    fun returnAvatarColor(component: String): Int {
+
+        val strippedString = component
+            .replace("[", "")
+            .replace("]", "")
+            .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedString)
+        if (scanner.hasNext()) {
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+        return Color.rgb(r, g, b)
+    }
+
+    fun logOut() {
+
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
 
 }
